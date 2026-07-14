7375db642a53e418839860c81ad61e5f0c287896
onety77 <aminualimy@gmail.com>
2026-07-14 06:09:02 +0100

    ca

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@
 
 export const LINKS = {
   x: "https://x.com/feeless26?s=21", // TODO: real X / community link
-  chart: "#", // TODO: real DexScreener chart link
+  chart: "https://dexscreener.com/robinhood/0xe0bc9f4fee753f35a928c64b4f039c03e7ed4ab4b24964f3f54f4e0479959abe", // TODO: real DexScreener chart link
 };
 
 // The token's contract address. Shown + copyable across the site.
-export const CA = "coming soon";
+export const CA = "0x69aecffcc7269d5163aacabe93168406d60dcae9";
